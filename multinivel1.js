f527aa6c478b2beffcbd619d9a0acecc6dacf81e
MauricioBTGIT <109316050+MauricioBTGIT@users.noreply.github.com>
2022-07-21 17:20:01 -0400

    Update multinivel1.js

--- multinivel1.js
+++ multinivel1.js
@@ -1,37 +1,52 @@
 var subjectObject = {
-  "Front-end": {
-    "HTML": ["Links", "Images", "Tables", "Lists"],
-    "CSS": ["Borders", "Margins", "Backgrounds", "Float"],
-    "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]    
+  "CONECTA": {
+    "Escucha Oferta": ["Agenda Llamada", "Acepta", "Ya no es cliente WOM", "Desconfia de la Llamada", "Cliente Empresa", "Línea como prepago", "Se portara a otra compañia", "Cesante o con problemas económicos", "Ya cuenta con plan de la nueva oferta", "Conforme con su plan actual/No le interesa", "Prefiere Gestionar en Sucursal", "Solicita Baja de Servicio", "Cliente Presenta Deuda con WOM", "Cliente con equipo en Arriendo"],
+    "No Escucha Oferta": ["Usuario Menor de Edad sin contacto con titular", "Inubicable por horario", "Solo en Horario PM", "Solo en horario AM"]
   },
-  "Back-end": {
-    "PHP": ["Variables", "Strings", "Arrays"],
-    "SQL": ["SELECT", "UPDATE", "DELETE"]
+  "NO CONECTA": {
+    "No Contesta": ["-"],
+    "Ocupado": ["-"],
+	"Buzón de Voz": ["-"],
+	"Congestionado": ["-"],
+	"Sin Ringback": ["-"],
+	"Número Erróneo": ["-"]
   }
 }
+
+
 window.onload = function() {
-  var subjectSel = document.getElementById("subject");
-  var topicSel = document.getElementById("topic");
-  var chapterSel = document.getElementById("chapter");
+  var lvl1Sel = document.getElementById("lvl1");
+  var lvl2Sel = document.getElementById("lvl2");
+  var lvl3Sel = document.getElementById("lvl3");
   for (var x in subjectObject) {
-    subjectSel.options[subjectSel.options.length] = new Option(x, x);
+    lvl1Sel.options[lvl1Sel.options.length] = new Option(x, x);
   }
-  subjectSel.onchange = function() {
-    //empty Chapters- and Topics- dropdowns
-    chapterSel.length = 1;
-    topicSel.length = 1;
+  
+  
+  
+  lvl1Sel.onchange = function() {
+    //empty Lvl2 y Lvl3 dropdowns
+    lvl3Sel.length = 1;
+    lvl2Sel.length = 1;
     //display correct values
     for (var y in subjectObject[this.value]) {
-      topicSel.options[topicSel.options.length] = new Option(y, y);
+      lvl2Sel.options[lvl2Sel.options.length] = new Option(y, y);
     }
   }
-  topicSel.onchange = function() {
-    //empty Chapters dropdown
-    chapterSel.length = 1;
+  lvl2Sel.onchange = function() {
+    //empty Lvl3 dropdown
+    lvl3Sel.length = 1;
     //display correct values
-    var z = subjectObject[subjectSel.value][this.value];
-    for (var i = 0; i < z.length; i++) {
-      chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
-    }
+    var z = subjectObject[lvl1Sel.value][this.value];
+	if (z.length == 1){
+		//alert("1 elemento");
+		lvl3Sel.options[lvl3Sel.options.length] = new Option(z[0], z[0])
+		lvl3Sel.selectedIndex = 1;
+	}
+	else{
+		for (var i = 0; i < z.length; i++) {
+		  lvl3Sel.options[lvl3Sel.options.length] = new Option(z[i], z[i]);
+		}
+	}
   }
 }
