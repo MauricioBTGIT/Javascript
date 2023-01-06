479bbdf8256052a9f006e98d263ab508db23b168
MauricioBTGIT <109316050+MauricioBTGIT@users.noreply.github.com>
2023-01-06 14:41:11 -0300

    Update and rename test23.js to test24.js

--- test24.js
+++ test24.js
@@ -6,8 +6,8 @@ function dynamicallyLoadScript(url) {
 	
     // Then bind the event to the callback function.
     // There are several events for cross browser compatibility.
-    script.onreadystatechange = callback;
-    script.onload = callback;
+    //script.onreadystatechange = callback;
+    //script.onload = callback;
 
     // Fire the loading
     head.appendChild(script);
