1591c2816c091582a22dcfebcb0f9152f6056359
MauricioBTGIT <109316050+MauricioBTGIT@users.noreply.github.com>
2023-01-06 14:43:05 -0300

    Update and rename test24.js to test25.js

--- test25.js
+++ test25.js
@@ -1,5 +1,6 @@
 function dynamicallyLoadScript(url) {
-    var head = document.head;
+    //var head = document.head;
+    var body = document.body;	
     var script = document.createElement("script");  // create a script DOM node
     script.type = 'text/javascript';
     script.src = url;  // set its src to the provided URL
@@ -10,7 +11,8 @@ function dynamicallyLoadScript(url) {
     //script.onload = callback;
 
     // Fire the loading
-    head.appendChild(script);
+    //head.appendChild(script);
+	body.appendChild(script);
     //document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
     //document.body.appendChild(script);
 }
