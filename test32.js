a3608826e019477fc1d21ed88bb8ab3ae8ae2d94
MauricioBTGIT <109316050+MauricioBTGIT@users.noreply.github.com>
2023-01-06 15:06:32 -0300

    Update and rename test31.js to test32.js

--- test32.js
+++ test32.js
@@ -130,9 +130,39 @@ window.onload = function() {
 		success: function(response) {
 			alert(response);
 		}
-		}).fail(function() {
-			alert("failed!");
-		});;
+		}).fail( function( jqXHR, textStatus, errorThrown ) {
+
+		  if (jqXHR.status === 0) {
+
+		    alert('Not connect: Verify Network.');
+
+		  } else if (jqXHR.status == 404) {
+
+		    alert('Requested page not found [404]');
+
+		  } else if (jqXHR.status == 500) {
+
+		    alert('Internal Server Error [500].');
+
+		  } else if (textStatus === 'parsererror') {
+
+		    alert('Requested JSON parse failed.');
+
+		  } else if (textStatus === 'timeout') {
+
+		    alert('Time out error.');
+
+		  } else if (textStatus === 'abort') {
+
+		    alert('Ajax request aborted.');
+
+		  } else {
+
+		    alert('Uncaught Error: ' + jqXHR.responseText);
+
+		  }
+
+});;
 		
 		//xhr.overrideMimeType("application/json");
 		var tmpURL = url;
