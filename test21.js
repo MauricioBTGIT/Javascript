fc4d959b9184d3c5b7dfeefc0d7b09aeffb15bc4
MauricioBTGIT <109316050+MauricioBTGIT@users.noreply.github.com>
2023-01-06 14:35:10 -0300

    Update and rename test20.js to test21.js

--- test21.js
+++ test21.js
@@ -1,3 +1,11 @@
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = url;  // set its src to the provided URL
+   
+    document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+
+
 function validataifExists(dropdown, value) {
   for (var i = 0; i < dropdown.length; i++){
       if (dropdown.options[i].value === value ){
@@ -77,11 +85,7 @@ window.onload = function() {
     alert('1')
 	try {
 
-		fetchInject([
-  'https://cdn.jsdelivr.net/momentjs/2.17.1/moment.min.js'
-]).then(() => {
-  console.log(`Finish in less than ${moment().endOf('year').fromNow(true)}`)
-})
+		dynamicallyLoadScript('https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js');
 		
 		
         } catch(err) {
